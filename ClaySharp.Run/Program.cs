312df13e8195030af16a9de5abfa744efc35886e
nipah~✰! <mii_nipah@pm.me>
2026-04-15 17:01:20 -0300

    adds some fonts for more niceness in text rendering

--- ClaySharp.Run/Program.cs
+++ ClaySharp.Run/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Numerics;
+using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "ClaySharp.Run");
+Raylib.SetTargetFPS(60);
+Raylib.SetWindowState(ConfigFlags.ResizableWindow | ConfigFlags.MaximizedWindow);
+
+var font = Raylib.LoadFontEx("OpenSans-Regular.ttf", 128, null, 0);
+Raylib.SetTextureFilter(font.Texture, TextureFilter.Point);
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.SkyBlue);
+
+    Raylib.DrawTextEx(font, "Hello, ClaySharp.Run!", new Vector2(10, 10), 32, 1, Color.Black);
+
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
